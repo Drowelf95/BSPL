cbc775293a84a7fd8a18aed086e264dc1fa3155a
Drowelf95 <sutter.johan@gmail.com>
2020-09-15 09:01:27 +0200

    Update gitignore

--- Backup/Tuto/config/dev.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const DB_HOST = 'mysql:host=db5000853620.hosting-data.io;dbname=dbs751772;charset=utf8';
-const DB_USER = 'dbu210283';
-const DB_PASS = 'Ple@seG1veM3in#1412';
--- config/dev.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const DB_HOST = 'mysql:host=db5000871579.hosting-data.io;dbname=dbs766481;charset=utf8';
-const DB_USER = 'dbu713039';
-const DB_PASS = 'Ple@seG1veM3in#1412';
